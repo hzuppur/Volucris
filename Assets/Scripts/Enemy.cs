5ff438f42fe5cf062b88cf86b66980c0519bcd74
Rengur <rednarest@gmail.com>
2020-10-24 11:55:53 +0300

    Basic enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,35 @@ public class Enemy : MonoBehaviour
 {
     public int health = 100;
     public GameObject deathEffect;
+    public float Speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new Vector3(Speed, 0, 0);
+        transform.position += movement * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyMovementBlocker")
+        {
+            Speed = Speed * -1;
+            transform.Rotate(0, 180, 0);
+        }
+
+        PlayerMovement player = collision.GetComponent<PlayerMovement>();
+        if (player != null)
+        {
+            //player.takeDamage();
+        }
     }
 
     public void TakeDamage(int damage){
