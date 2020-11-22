8d80ed4f7bbc42b4465b27068d67cae44b3e9166
Risto-R <rahulan.risto@gmail.com>
2020-11-22 22:06:47 +0200

    Added a door and a key

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private float _startY;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        _startY = gameObject.transform.position.y;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        Vector3 gameObjPos = gameObject.transform.position;
+        float newYpos = (float) (_startY + Math.Cos(Time.time * 4) / 4f);
+        gameObject.transform.position = new Vector3(gameObjPos.x, newYpos, gameObjPos.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Player"){
+            Destroy(this.transform.parent.gameObject);
+        }
+    }
+}
